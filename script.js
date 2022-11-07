0f68d4dfe7952c8ed19584c7e83fa104c003645f
JhonatanFaelis <dev.jhonatan.faelis@gmail.com>
2022-11-07 18:45:06 -0300

    menu responsivo

--- script.js
+++ script.js
@@ -42,5 +42,22 @@ control.addEventListener('click', () =>{
 });    
 });
 
+
+
+
+
 })
 
+/*MENU RESPONSIVO*/
+
+function menuShow(){
+
+    let menuMobile = document.querySelector('.mobile-menu')
+        if (menuMobile.classList.contains('open')) {
+            menuMobile.classList.remove('open')
+            document.querySelector('.icon').src ="./img/menu_white_36dp.svg"
+        } else {
+            menuMobile.classList.add('open')
+            document.querySelector('.icon').src ="./img/close_white_36dp.svg"
+    }
+}
